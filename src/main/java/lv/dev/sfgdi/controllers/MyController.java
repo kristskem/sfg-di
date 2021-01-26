80b2578f845c45d04e7b8270737812334af3e93d
kkemers <kkemers@inbox.lv>
2021-01-26 22:00:35 +0200

    Add spring context.

---
+++ src/main/java/lv/dev/sfgdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package lv.dev.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHelloWorld() {
+        System.out.println("Hello world");
+
+        return "Hi folks!";
+    }
+}
