771f22c414ecb3c7d8639b0656e4dfff6f95cbd7
kkemers <kkemers@inbox.lv>
2021-01-30 11:38:13 +0200

    Add primary annotation.

--- src/main/java/lv/dev/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/lv/dev/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,6 @@
 package lv.dev.sfgdi.controllers;
 
 import lv.dev.sfgdi.services.GreetingService;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,7 +8,7 @@ public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(@Qualifier("greetingServiceImpl") GreetingService greetingService) {
+    public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- src/main/java/lv/dev/sfgdi/services/GreetingServiceImpl.java
+++ src/main/java/lv/dev/sfgdi/services/GreetingServiceImpl.java
@@ -1,12 +1,15 @@
 package lv.dev.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Service say - Hello world!";
+        return "Hello world from primary bean. I am the main greeting service agent, I will take care about our" +
+                " services if no one else was delegated";
     }
 }
